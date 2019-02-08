c44874d47d7e06544f9ca99c7caa6de45ef317f6
valentin.smirnov <valentin.smirnov@nevosoft.ru>
2019-02-08 12:54:53 +0300

    feat: add Firebase Cloud Messaging (FCM)

--- dependencies/android/src/org/haxe/extension/Firebase.java
+++ dependencies/android/src/org/haxe/extension/Firebase.java
@@ -5,14 +5,21 @@ import android.app.Application;
 import android.content.res.AssetManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ApplicationInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 
+import android.support.annotation.NonNull;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -45,6 +52,36 @@ public class Firebase extends Extension {
         return payloadBundle;
     }
 
+    // Get token
+    /*
+    * The registration token may change when:
+    * The app deletes Instance ID
+    * The app is restored on a new device
+    * The user uninstalls/reinstall the app
+    * The user clears app data.*/
+    public static String getInstanceIDToken()
+    {
+        final String token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "getInstanceId success: " + token);
+
+        return token;
+
+        /*
+        FirebaseInstanceId.getInstance().getInstanceId()
+        .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if (!task.isSuccessful()) {
+                    Log.d(TAG, "getInstanceId failed", task.getException());
+                    return;
+                }
+
+                Log.d(TAG, "getInstanceId success", task.getResult().getToken());
+            }
+        });
+        */
+    }
+
 
     public static void sendFirebaseAnalyticsEvent(String eventName, String jsonPayload) {
         Log.d(TAG, "Firebase.java: sendFirebaseAnalyticsEvent name= " + eventName + ", payload= " + jsonPayload);
@@ -75,13 +112,6 @@ public class Firebase extends Extension {
         firebaseAnalytics.setCurrentScreen(Extension.mainActivity, screenName, screenClass);
     }
 
-    public static String getInstanceIDToken() {
-        String idToken = FirebaseInstanceId.getInstance().getToken();
-        Log.d(TAG, "Firebase.java: getInstanceIDToken= " + idToken);
-
-        return idToken;
-    }
-
     public static void setUserID(String userID) {
         Log.d(TAG, "Firebase.java: setUserID id= " + userID);
 
@@ -99,14 +129,49 @@ public class Firebase extends Extension {
      */
     public void onCreate (Bundle savedInstanceState) {
 
-        Log.d(TAG, "Firebase.java: onCreate ");
+        Log.d(TAG, "Firebase extension onCreate ");
+
+        // Handle possible data accompanying notification message.
+        Intent intent = null;
+		try {
+			PackageManager pm = mainContext.getPackageManager();
+			if(pm != null) {
+				String packageName = mainContext.getPackageName();
+				intent = pm.getLaunchIntentForPackage(packageName);
+				intent.addCategory(Intent.CATEGORY_LAUNCHER); // Should already be set, but just in case
+			}
+		} catch (Exception e) {
+			Log.d(TAG, "Failed to get application launch intent");
+		}
+        
+        Bundle intentBundle = null;
+        if (intent != null && intent.getExtras() != null) {
+            intentBundle = intent.getExtras();
+            for (String key : intentBundle.keySet()) {
+                Object value = intentBundle.get(key);
+                Log.d(TAG, "Launch intent Key: " + key + " Value: " + value);
+            }
+        }
+
+        // subscribe for new messages
+        FirebaseMessaging.getInstance().subscribeToTopic("news")
+        .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                String msg = "Successful Subscribed messages from Firebase";
+                if (!task.isSuccessful()) {
+                    msg = "Failed Subscribed messages from Firebase";
+                }
+                Log.d(TAG, msg);
+            }
+        });
 
     }
 
 
-        /**
-         * Perform any final cleanup before an activity is destroyed.
-         */
+    /**
+     * Perform any final cleanup before an activity is destroyed.
+     */
     public void onDestroy () {
 
 
